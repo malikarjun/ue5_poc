fb4c0c68daf3abd59e366010c2685fa5fe9cbf2d
malikarjun <mswami032@gmail.com>
2022-12-07 18:26:37 -0800

    create a single script

--- xml_to_gltf.py
+++ xml_to_gltf.py
@@ -1,27 +1,228 @@
 import time
-
 import untangle
 import os
 from os import makedirs
 from os.path import basename, dirname, join
-from gltf_utils import load_gltf, merge_gltfs
 import numpy as np
 import shutil
-
+import struct
 import subprocess
 import argparse
-
-from pygltflib import TextureInfo, Texture, Sampler, Node
+from pygltflib import GLTF2, TextureInfo, Texture, Sampler, Node
 from pygltflib.utils import ImageFormat, Image
-
-from utils import scale_mat, rotate_mat, translate_mat, column_order
-from utils import bounding_box
-
 
 
 TMP_GLTF_DIR = "gltfs"
 AREA_LIGHT_EXTENSION = "KHR_lights_area"
 ENV_MAP_EXTENSION = "KHR_lights_environment"
+
+
+# https://ai.stackexchange.com/questions/14041/how-can-i-derive-the-rotation-matrix-from-the-axis-angle-rotation-vector
+def rotate_mat(axis_angle):
+	k = axis_angle[1:]
+	kx, ky, kz = k
+	angle = np.deg2rad(axis_angle[0])
+
+	ct = np.cos(angle)
+	st = np.sin(angle)
+
+	mat = np.matrix([
+		[ct + kx ** 2 * (1 - ct), kx * ky * (1 - ct) - kz * st, kx * kz * (1 - ct) + ky * st, 0.],
+		[ky * kx * (1 - ct) + kz * st, ct + ky ** 2 * (1 - ct), ky * kz * (1 - ct) - kx * st, 0.],
+		[kz * kx * (1 - ct) - ky * st, kz * ky * (1 - ct) + kx * st, ct + kz ** 2 * (1 - ct), 0.],
+		[0., 0., 0., 1.]
+	])
+
+	return mat
+
+
+def translate_mat(translate):
+	mat = np.eye(4)
+	for i in range(3):
+		mat[i, 3] = translate[i]
+
+	return mat
+
+
+def scale_mat(scale):
+	mat = np.eye(4)
+	for i in range(3):
+		mat[i, i] = scale[i]
+	return mat
+
+
+def column_order(matrix, dtype="str"):
+	data = []
+	r, c = matrix.shape
+	for j in range(c):
+		for i in range(r):
+			if dtype == "str":
+				data.append("{:.5f}".format(matrix[i, j]))
+			else:
+				data.append(matrix[i, j])
+	return data
+
+
+def load_vertices(filename, model_mat=None):
+	gltf = GLTF2().load(filename)
+
+	# get the first mesh in the current scene (in this example there is only one scene and one mesh)
+	mesh = gltf.meshes[gltf.scenes[gltf.scene].nodes[0]]
+
+	# get the vertices for each primitive in the mesh (in this example there is only one)
+	for primitive in mesh.primitives:
+
+		# get the binary data for this mesh primitive from the buffer
+		accessor = gltf.accessors[primitive.attributes.POSITION]
+		bufferView = gltf.bufferViews[accessor.bufferView]
+		buffer = gltf.buffers[bufferView.buffer]
+		data = gltf.get_data_from_buffer_uri(buffer.uri)
+
+		# pull each vertex from the binary buffer and convert it into a tuple of python floats
+		vertices = []
+		for i in range(accessor.count):
+			index = bufferView.byteOffset + accessor.byteOffset + i*12  # the location in the buffer of this vertex
+			d = data[index:index+12]  # the vertex data
+			v = struct.unpack("<fff", d)   # convert from base64 to three floats
+
+			if model_mat is not None:
+				vert_homo = np.ones(4)
+				vert_homo[:3] = v
+				vert_homo = model_mat @ vert_homo
+				vert_homo = np.squeeze(np.array(vert_homo))
+				v = vert_homo[:3]
+
+			vertices.append(v)
+
+	# convert a numpy array for some manipulation
+	vertices = np.array(vertices)
+	return vertices
+
+
+def bounding_box(filename, model_mat=None):
+	points = load_vertices(filename=filename, model_mat=model_mat)
+
+	min_pt = np.min(points, axis=0)
+	max_pt = np.max(points, axis=0)
+	center = (min_pt + max_pt)/2
+	radius = (max_pt - min_pt)/2
+	return center, radius
+
+
+def load_gltf(filename):
+	return GLTF2().load(filename)
+
+def get_scene_nodes(gltf):
+	return gltf.scenes[gltf.scene].nodes
+
+def merge_gltf(file1, file2):
+	gltf1 = load_gltf(file1)
+	gltf2 = load_gltf(file2)
+
+	num_scene_nodes = len(get_scene_nodes(gltf1))
+	num_meshes = len(gltf1.meshes)
+	num_accessors = len(gltf1.accessors)
+	num_buffer_views = len(gltf1.bufferViews)
+	num_buffers = len(gltf1.buffers)
+	num_materials = len(gltf1.materials)
+	num_textures = len(gltf1.textures)
+	num_images = len(gltf1.images)
+	num_samplers = len(gltf1.samplers)
+
+	# add extensions
+
+	gltf1.extensions = gltf1.extensions | gltf2.extensions
+	gltf1.extensionsUsed = list(set().union(*[gltf1.extensionsUsed, gltf2.extensionsUsed]))
+	gltf1.extensionsRequired = list(set().union(*[gltf1.extensionsRequired, gltf2.extensionsRequired]))
+
+	# add scene nodes
+	for scene_node in get_scene_nodes(gltf2):
+		scene_node += num_scene_nodes
+		get_scene_nodes(gltf1).append(scene_node)
+
+	# add nodes
+	for node in gltf2.nodes:
+		if node.mesh is not None:
+			node.mesh += num_meshes
+		gltf1.nodes.append(node)
+
+	# add meshes
+	for mesh in gltf2.meshes:
+		for primitive in mesh.primitives:
+			primitive.material += num_materials
+
+			primitive.indices += num_accessors
+			if primitive.attributes.POSITION is not None:
+				primitive.attributes.POSITION += num_accessors
+			if primitive.attributes.NORMAL is not None:
+				primitive.attributes.NORMAL += num_accessors
+			if primitive.attributes.TEXCOORD_0 is not None:
+				primitive.attributes.TEXCOORD_0 += num_accessors
+			if primitive.attributes.TEXCOORD_1 is not None:
+				primitive.attributes.TEXCOORD_1 += num_accessors
+		gltf1.meshes.append(mesh)
+
+	# add accessors
+	for accessor in gltf2.accessors:
+		accessor.bufferView += num_buffer_views
+		gltf1.accessors.append(accessor)
+
+	# add buffer views
+	for buffer_view in gltf2.bufferViews:
+		buffer_view.buffer += num_buffers
+		gltf1.bufferViews.append(buffer_view)
+
+	# add buffers
+	for buffer in gltf2.buffers:
+		gltf1.buffers.append(buffer)
+
+	# add textures
+	for texture in gltf2.textures:
+		texture.sampler += num_samplers
+		texture.source += num_images
+		gltf1.textures.append(texture)
+
+	# add samplers
+	for sampler in gltf2.samplers:
+		gltf1.samplers.append(sampler)
+
+	# add images
+	for image in gltf2.images:
+		gltf1.images.append(image)
+
+	# add materials
+	for material in gltf2.materials:
+		if material.normalTexture is not None:
+			material.normalTexture.index += num_textures
+		if material.pbrMetallicRoughness is not None:
+			if material.pbrMetallicRoughness.baseColorTexture is not None:
+				material.pbrMetallicRoughness.baseColorTexture.index += num_textures
+			if material.pbrMetallicRoughness.metallicRoughnessTexture is not None:
+				material.pbrMetallicRoughness.metallicRoughnessTexture.index += num_textures
+		gltf1.materials.append(material)
+
+	gltf1.convert_images(ImageFormat.DATAURI)
+
+	return gltf1
+
+
+def merge_gltfs(files):
+	_merged_file = None
+	try:
+		_merged = merge_gltf(files[0], files[1])
+		_merged_file = join(dirname(files[0]), "_merged.gltf")
+		_merged.save(_merged_file)
+
+		for file in files[2:]:
+			_merged = merge_gltf(_merged_file, file)
+			_merged.save(_merged_file)
+
+		gltf = load_gltf(_merged_file)
+	finally:
+		if _merged_file is not None:
+			os.remove(_merged_file)
+
+	return gltf
 
 
 def get_elem_val(element, atype, aselect_tag, aselect_tag_value, atag=None):
@@ -104,26 +305,13 @@ def parse_axis_angle(xml):
 	return vals
 
 def handle_scales(scales):
-	# mat = np.eye(4)
-	# for scale in scales:
-	# 	mat = scale_mat(parse_xyz(scale)) @ mat
-	# return mat
-
 	return [scale_mat(parse_xyz(scale)) for scale in scales]
 
 def handle_rotates(rotates):
-	# mat = np.eye(4)
-	# for rotate in rotates:
-	# 	mat = rotate_mat(parse_axis_angle(rotate)) @ mat
-	# return mat
 	return [rotate_mat(parse_axis_angle(rotate)) for rotate in rotates]
 
 
 def handle_translates(translates):
-	# mat = np.eye(4)
-	# for translate in translates:
-	# 	mat = translate_mat(parse_xyz(translate)) @ mat
-	# return mat
 	return [translate_mat(parse_xyz(translate)) for translate in translates]
 
 def matrix_mul(mats):
@@ -281,8 +469,4 @@ if __name__ == "__main__":
 
 	merged_gltf = parse_xml(input_file, output_file)
 	merged_gltf.save(output_file)
-
-
-	# merged_gltf = parse_xml("siggraphasia20dataset/code/Routine/scenes/xml/scene0065_01/main.xml")
-	# merged_gltf.save("merged_final.gltf")
 
